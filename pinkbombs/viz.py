78e6927678563d0641d1bb0020985458025c70ba
gmguarino <gmguarino1@gmail.com>
2024-02-29 19:17:51 +0100

    change scatter plot geo

--- pinkbombs/viz.py
+++ pinkbombs/viz.py
@@ -1,31 +1,32 @@
 import altair as alt
 import pandas as pd
 import plotly.express as px
+from plotly.graph_objects import Figure
 
 
-def make_geo(input_df, input_id, input_column, input_color_theme):
+def make_geo(
+    input_df: pd.DataFrame, input_id: str, input_column: str, input_color_theme: str,
+) -> Figure:
     geo = px.scatter_geo(
         input_df,
         locations=input_id,
         color=input_column,
         locationmode="country names",
-        size=input_column,
+        size=input_df[input_column], #pd.Series([100]*input_df.shape[0]), #input_df[input_column],
         color_continuous_scale=input_color_theme,
         range_color=(0, max(input_df["Tonnes - live weight"])),
         projection="natural earth",
         labels={"Tonnes - live weight": "Tonnes"},
     )
     geo.update_layout(
-        template="plotly_dark",
-        plot_bgcolor="rgba(0, 0, 0, 0)",
-        paper_bgcolor="rgba(0, 0, 0, 0)",
+        template="plotly",
         margin={"l": 0, "r": 0, "t": 0, "b": 0},
-        height=350,
+        height=500,
     )
     return geo
 
 
-def make_area_chart(input_df, input_x, input_y, input_color_theme):
+def make_area_chart(input_df: pd.DataFrame, input_x: str, input_y: str) -> Figure:
     group_df = input_df[[input_x, input_y]].groupby(input_x).sum().reset_index()
     group_df = group_df.sort_values(by=input_y, ascending=False).iloc[:5]
     area = px.area(
@@ -35,19 +36,12 @@ def make_area_chart(input_df, input_x, input_y, input_color_theme):
         color="Country Name En",
         line_group="Country Name En",
     )
-
-    px.bar(
-        group_df,
-        x=input_x,
-        y=input_y,
-        color=input_y,
-        color_continuous_scale=input_color_theme,
-        log_y=True,
-    )
     return area
 
 
-def make_donut(input_response, input_response_perc, input_text, input_color):
+def make_donut(
+    input_response: str, input_response_perc: float, input_text: str, input_color: str,
+) -> alt.Chart:
     if input_color == "blue":
         chart_color = ["#29b5e8", "#155F7A"]
     if input_color == "green":
@@ -61,13 +55,13 @@ def make_donut(input_response, input_response_perc, input_text, input_color):
         {
             "Topic": ["", input_text],
             "% value": [100 - input_response_perc, input_response_perc],
-        }
+        },
     )
     source_bg = pd.DataFrame(
         {
             "Topic": ["", input_text],
             "% value": [100, 0],
-        }
+        },
     )
 
     plot = (
