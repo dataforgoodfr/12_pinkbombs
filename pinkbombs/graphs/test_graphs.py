cb940d9f30be27bb62f85f283df689fc3f196ad6
Sarah Rieubland <sarahrieubland@yahoo.co.uk>
2024-04-10 13:38:49 +0200

    function for graph 1.2 with data cleaning included

--- pinkbombs/graphs/test_graphs.py
+++ pinkbombs/graphs/test_graphs.py
@@ -15,3 +15,16 @@ g1_1 = pb.make_area_single_chart(
     )  
 
 g1_1.write_html("pinkbombs/graphs/test_html/"+data1_1_name+".html")
+
+# Graph 1.2 - Wild Altantic salmon collapse
+data1_2_name = 'hyper_growth_salmon_farming_1.2'
+data1_2_file = "data/"+data1_2_name+".csv" 
+df_data1_2 = pd.read_csv(data1_2_file)
+
+g1_2 = pb.make_area_order_chart(
+    df_data1_2, 'Year', 
+    'Tonnes - live weight', 'Country', 
+    title='Hyper-growth in salmon farming', 
+    reorder=True)
+
+g1_2.write_html("pinkbombs/graphs/test_html/"+data1_2_name+".html")
--- pinkbombs/graphs/viz.py
+++ pinkbombs/graphs/viz.py
@@ -39,3 +39,41 @@ def make_area_single_chart(input_df, input_x, input_y, title,
     area.update_layout(template=theme, title=title)
     area.update_yaxes(exponentformat="none")
     return area
+
+
+def make_area_order_chart(input_df, input_x, input_y, input_col, title, reorder=False, 
+                          palette=px.colors.qualitative.Pastel,
+                          theme='simple_white') -> Figure:
+    """Returns plotly express object as area chart with multiple lines
+            Parameters:
+                    input_df (pd.DataFrame): dataframe with data to be visualised 
+                    input_x (str): name of the field for the x axis
+                    input_y (str): name of the field for the y axis
+                    input_col (str): name of the field for the colors
+                    title (str): chart title
+                    reorder (boolean): if true, dataframe is reodered by the input_col field, defauls is False 
+                    palette (px.object): plotly discrete palette, default is Pastel
+                    theme (str): plotly chart theme, default is 'simple_white'
+            Returns:
+                    area (plotly object): output chart object
+    """ 
+    # Data cleaning - TO REMOVE
+    input_df[input_y] = input_df[input_y].str.replace(',', '.')
+    input_df[input_y] = input_df[input_y].astype(float)
+
+    if reorder:
+        input_df_agg = input_df.groupby(input_col)[input_y].sum().sort_values(ascending=False)
+        myorder = input_df_agg.reset_index()[input_col].tolist()
+        input_df_u = input_df.set_index(input_col)
+        input_df = input_df_u.loc[myorder].reset_index()
+
+    area   = px.area(
+        input_df,
+        x=input_x,
+        y=input_y,
+        color=input_col,
+        color_discrete_sequence=palette,
+        )
+    area.update_layout(template=theme, title=title)
+    area.update_yaxes(exponentformat="none")
+    return area
