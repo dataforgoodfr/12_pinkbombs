db6818c70ee7b931e70d9caca4dba7ba062f1146
gmguarino <gmguarino1@gmail.com>
2024-04-18 11:32:23 +0200

    add: cors middleware

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, Depends
-from pinkbombs import api, verify_token
+from fastapi.middleware.cors import CORSMiddleware
 
+from pinkbombs import api, verify_token
+from pinkbombs.auth.cors import origins
+
+app = FastAPI()
 GRAPHS = ["aqua-tonnes"]
 TOKEN = "pinkbombs"
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     api.router,
---
+++ pinkbombs/auth/cors.py
@@ -0,0 +1,5 @@
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
