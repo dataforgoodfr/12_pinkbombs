4bddb911ba60213d5b98dc706e0a667ea7d9da4e
gmguarino <gmguarino1@gmail.com>
2024-05-26 18:35:22 +0200

    generate graphs

--- pinkbombs/generate.py
+++ pinkbombs/generate.py
@@ -1,14 +1,13 @@
-from config import MAPPING, MAPS
-import argparse
+from config import MAPPING, MAPS, MAPPINGFR, MAPSFR
+import json
 
-
-def generate_graph(graph_name):
-    if graph_name not in MAPPING:
+def generate_graph(graph_name, mapping):
+    if graph_name not in mapping:
         raise ValueError(f"Graph '{graph_name}' not found")
-    df = MAPPING[graph_name]["parser"](
-        "data/" + MAPPING[graph_name]["filename"],
+    df = mapping[graph_name]["parser"](
+        "data/" + mapping[graph_name]["filename"],
     )
-    chart_obj = MAPPING[graph_name]["function"](df, *MAPPING[graph_name]["arguments"])
+    chart_obj = mapping[graph_name]["function"](df, *mapping[graph_name]["arguments"])
     chart_obj.update_layout(
         hoverlabel=dict(
             bgcolor="white",
@@ -17,38 +16,40 @@ def generate_graph(graph_name):
     return chart_obj.to_json()
 
 
-def generate_maps(map_name):
-    if map_name not in MAPS:
+def generate_map(map_name, mapping):
+    if map_name not in mapping:
         raise ValueError(f"Map '{map_name}' not found")
-    df = MAPS[map_name]["parser"](
-        "data/" + MAPS[map_name]["filename"],
+    df = mapping[map_name]["parser"](
+        "data/" + mapping[map_name]["filename"],
     )
-    html_map = MAPS[map_name]["function"](df, *MAPS[map_name]["arguments"])
+    html_map = mapping[map_name]["function"](df, *mapping[map_name]["arguments"])
     return html_map
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--root-dir", default="./", help="Directory where to save the generated data")
-    parser.add_argument("--graph-data", default="*", help="Graphs to generate, default is all graphs")
-    parser.add_argument("--map-data", default="*", help="Maps to generate, default is all maps")
-    
-    args = parser.parse_args()
+    for graph_name in MAPPING:
+        print(graph_name)
+        graph_json = generate_graph(graph_name, MAPPING)
+        with open(f"data/graphs/en/{graph_name}.json", "w") as f:
+            json.dump(graph_json, f)
 
-    inverse_mapping = {v["filename"]: k for k, v in MAPPING.items()}
-    
-    if args.graph_data == "*":
-        new_graphs = list(MAPPING.keys())
-    else:
-        new_graphs = [inverse_mapping[filename] for filename in args.graph_data.split(",")]
+    for graph_name in MAPPINGFR:
+        print(graph_name)
+        graph_json = generate_graph(graph_name, MAPPINGFR)
+        with open(f"data/graphs/fr/{graph_name}.json", "w") as f:
+            json.dump(graph_json, f)
 
-    if args.map_data == "*":
-        new_maps = list(MAPS.keys())
-    else:
-        new_maps = [inverse_mapping[filename] for filename in args.map_data.split(",")]
-    
-    print("new_graphs", new_graphs)
-    print("new_maps", new_maps)
+    for map_name in MAPS:
+        print(map_name)
+        map_json = generate_map(map_name, MAPS)
+        with open(f"data/maps/en/{map_name}.html", "w") as f:
+            f.write(map_json)
+
+    for map_name in MAPSFR:
+        print(map_name)
+        map_json = generate_map(map_name, MAPSFR)
+        with open(f"data/maps/fr/{map_name}.html", "w") as f:
+            f.write(map_json)
 
     
     
--- pinkbombs/graphs/__init__.py
+++ pinkbombs/graphs/__init__.py
@@ -1,4 +1,4 @@
-from .viz import make_area_chart, make_area_single_chart
+from .viz import make_area_chart, make_area_single_chart, make_area_chart_options
 from .viz import make_area_order_chart, make_bar_chart, make_area_order_chart_grouped
 from .viz import make_color_bar_chart, make_color_bar_chart2
 from .viz import make_animated_bubble_map, make_treemap_chart
