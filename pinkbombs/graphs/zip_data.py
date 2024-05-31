8e17f14f0e440bc9b2fbc500f0acac5228acba54
Sarah Rieubland <sarahrieubland@yahoo.co.uk>
2024-05-31 16:33:10 +0200

    csv files zipped

--- pinkbombs/graphs/zip_data.py
+++ pinkbombs/graphs/zip_data.py
@@ -1,9 +1,14 @@
 
 import pandas as pd
+import os
 
-data1_0_name = 'numbers_salmons_farmed_1.0'
-data1_0_file = "data/"+data1_0_name+".csv"
-df_data1_0 = pd.read_csv(data1_0_file)
+# Script to read the csv files and save them as zip for download from pinkbombs.org
 
-df_data1_0.to_csv("download/csv/"+data1_0_name+".csv.zip", 
-                  compression='zip')
+path = "data/"
+dir_list = os.listdir(path)
+
+for el in dir_list:
+    if el[-3:] == 'csv':
+        df = pd.read_csv(path+el)
+        df.to_csv("download/csv/"+el+".zip", compression='zip')
+        print("Files converted: ", el)
